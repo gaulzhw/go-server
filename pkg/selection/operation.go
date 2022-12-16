6a7b6335173bece55133f6da2af3627f857f1491
gzw <zhiwen.gaul@gmail.com>
2022-12-16 16:04:32 +0800

    add files and selection

---
+++ pkg/selection/operation.go
@@ -0,0 +1,18 @@
+package selection
+
+// Operator represents a key/field's relationship to value(s).
+// See labels.Requirement and fields.Requirement for more details.
+type Operator string
+
+// Defines some operators to use.
+const (
+	DoesNotExist Operator = "!"
+	Equals       Operator = "="
+	DoubleEquals Operator = "=="
+	In           Operator = "in"
+	NotEquals    Operator = "!="
+	NotIn        Operator = "notin"
+	Exists       Operator = "exists"
+	GreaterThan  Operator = "gt"
+	LessThan     Operator = "lt"
+)
